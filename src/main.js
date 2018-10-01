9d6d06e99473d6b1ae4552dd168436f57a5e9677
AJ Mcgladdery, Ben Kiggen and Colin Cook <ben@>
2018-10-01 15:00:34 -0700

    Make all the things work correctly.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import './sass/styles.scss';
-// import './journal.js';
+// import $ from jquery;
 //BACK END
 
-function Entry(title, content, date){
+function Entry(title, content){
   this.title = title;
   this.content = content;
-  this.date = date;
+  // this.date = date;
 }
 
-function makeEntry(title, content, date) {
-  var newEntry = new Entry(title, content, date);
+function makeEntry(title, content) {
+  var newEntry = new Entry(title, content);
   newEntry.words = newEntry.wordCount();
   newEntry.vowels = newEntry.vowelCount();
   newEntry.consonants = newEntry.consonantCount();
@@ -17,9 +17,9 @@ function makeEntry(title, content, date) {
   return newEntry;
 }
 
-var titleTest = "Today was Horrible";
-var contentTest = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt bunt in culpa qui officia deserunt mollit anim id est laborum.";
-var dateTest = "January 15th, 1984";
+// var titleTest = "Today was Horrible";
+// var contentTest = "Lorem ipsum dolor! sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt bunt in culpa qui officia deserunt mollit anim id est laborum.";
+// var dateTest = "January 15th, 1984";
 
 
 
@@ -39,17 +39,42 @@ Entry.prototype.consonantCount = function() {
 };
 
 Entry.prototype.getTeaser = function() {
-  var wordSplit = this.content.split(/[.!?]/gi);
-  var wordSplitArray = wordSplit[0].split();
-  var wordArray = wordSplitArray.slice(0, 8);
-  return wordArray.join(" ");
+
+//find the first eight words
+var firstEightArray = this.content.split(" ").slice(0, 8).join(" ").split("");
+//if first eight vontains puncation
+var wordIndex = firstEightArray.findIndex(wordRegex);
+function wordRegex(element){
+  var answer;
+  return answer === null ? 0 :  element.match(/[.!?]/gi);
 }
+if ( wordIndex === -1) {
+  return firstEightArray.join("");
+} else {
+  var wordArray = firstEightArray.slice(0, (wordIndex + 1));
+  return wordArray.join("");
+}
+};
 
 
 
 //FRONT END
 $(document).ready(function(){
-
-  var test = makeEntry(titleTest, contentTest, dateTest);
-  console.log(test);
+  $("#submit-button").click(function(event){
+    event.preventDefault();
+    var title = $("#title").val();
+    var content = $("#content").val();
+    var journalEntry = makeEntry(title, content);
+    $(".journal-container").append('<div class="journal-post card well">' + '<h1 class="title-post card-header">' + journalEntry.title +
+    '</h1><h2 class="teaser-post card-text">' +
+    journalEntry.teaser +
+     '</h2><p class="content-post card-text">' + journalEntry.content +
+      '</p><p class="word-count card-text">Words: ' +  journalEntry.words +
+      '</p><p class="vowel-count card-text">Vowels: ' + journalEntry.vowels +
+      '</p><p class="consonant-count card-text">Consonants: ' +
+      journalEntry.consonants +
+      '</p></div>');
+      $("#title").val('');
+      $("#content").val('');
+  });
 });
